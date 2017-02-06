7b916aa14081def988063cf6915c38a455e6a4dd
Adam Katz <github@khopis.com>
2017-02-05 22:51:42 -0800

    synced grc.fish to grc.bash and grc.zsh ... nothing fancy here, I don't know fish

--- grc.fish
+++ grc.fish
@@ -2,10 +2,13 @@
 # Main File: ~/.local/share/omf/pkg/grc/init.fish
 # echo $grc_plugin_execs
 
-set -U grc_plugin_execs cat cvs df diff dig gcc g++ ls ifconfig \
-               make mount mtr netstat ping ps tail traceroute \
-               wdiff \
-               blkid du docker docker-machine env ip iostat \
-               last lsattr lsblk lspci lsmod getfacl getsebool ulimit uptime nmap \
-               free semanage sar ss sysctl systemctl stat showmount tune2fs \
-               vmstat w who
+set -U grc_plugin_execs cat ls \
+               ant as blkid cc cvs c++ df diff dig dnf \
+               docker docker-compose docker-machine \
+               du env free gas gcc getfacl getsebool g++ gmake \
+               hd head hexdump ifconfig ioping iostat ip iptables \
+               journalctl last ld ldap lsattr lsblk lsmod lspci \
+               make mount mtr mvn netstat nmap oping ping ping6 \
+               ps sar semanage setsebool showmount ss stat \
+               sysctl systemctl tail tcpdump traceroute traceroute6 \
+               tune2fs ulimit uptime vmstat w who wdiff
